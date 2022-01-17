6ad1e4e820b6cc562caf6fa12c651e29019813f9
Saqlain Javed <sqchoudry@gmail.com>
2022-01-17 17:54:45 +0500

    css changes

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ function calculate_loan(){
     const _m = loan_period*12;
     const _1mi = amount/_m;
     const _i = _1mi*installments
-    document.getElementById("installment").innerText = `Your each installment will be ${_i}.`
+    document.getElementById("installment").innerText = `Your each installment will be ${_i.toFixed(2)}.`
 }
 
 function check_amount(){
     
 }
-
+function myFunction() {
+    document.getElementById("apply").innerHTML = "Congratulations! you have successfully applied for loan if you will be illigeable the loan amount will be transferred to the given account number.";
+  }
 function showError(id, message){
     document.getElementById("error").innerText = "red"
 }
